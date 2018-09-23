4bd770063873ad3f2e204426dda5c4a67a96c1aa
denisb <denis.bouhineau@imag.fr>
2018-09-23 12:04:12 +0200

    ajout version -02

--- src/main/cpp/native-lib.cpp
+++ src/main/cpp/native-lib.cpp
@@ -1,17 +1,17 @@
 #include <jni.h>
 #include <string>
 
+
 extern "C" JNIEXPORT jstring
 JNICALL
-Java_com_example_bouhineau_myapplication_MainActivity_stringFromJNICalcReg(
+Java_edu_bdenis_gperf_MainActivity_stringFromJNICalcReg(
         JNIEnv *env,
         jobject /* this */,
         int pTailleDonnees) {
-    int n;
-	//int i, r;
+    int i,n,r;
     n=1;
-    //r=n;
-    char str[20];
+    r=n;
+    char str[20]; // asm ou __asm__ ou __asm // et volatile ?
     __asm(
     "ADD R6, %1, #1;"
     "MOV R7, #1;"
@@ -48,39 +48,45 @@ Java_com_example_bouhineau_myapplication_MainActivity_stringFromJNICalcReg(
     return env->NewStringUTF(hello.c_str());
 }
 
+
+
 extern "C" JNIEXPORT jstring
 JNICALL
-Java_com_example_bouhineau_myapplication_MainActivity_stringFromJNICalcVar02(
+Java_edu_bdenis_gperf_MainActivity_stringFromJNICalcVarOO(
         JNIEnv *env,
         jobject /* this */,
         int pTailleDonnees) {
     int n;
-	//int i, r;
+    int i, r;
     n=1;
+    r=n;
     char str[20];
     __asm(
     "ADD R6, %1, #1;" //i
-	"CMP R6, #0;" 
-	"BEQ .L2;"
-	"MOV R7, #1;" //n
+    "CMP R6, #0;"
+    "BEQ .L2;"
+    "MOV R7, #1;" //n
 ".L7:CMN R8, #34;" //r
-	"CMPNE R8, #1;"
-	"BEQ .L3;"
-	"CMN R8, #1;"
-	"CMNNE R8, #10;"
-	"BNE .L4"
-".L3:ADD R7, R7, #1;" 
-	"MOV R8, R7;" 
+    "IT ne;"
+    "CMPNE R8, #1;"
+    "BEQ .L3;"
+    "CMN R8, #1;"
+    "IT ne;"
+    "CMNNE R8, #10;"
+    "BNE .L4;"
+".L3:ADD R7, R7, #1;"
+    "MOV R8, R7;"
 ".L4:TST R8, #1;"
-	"ADDNE R8, R8, R8, asl #1;"
-	"ADDNE R8, R8, #1;"
-	"ADD R8, R8, R8, lsr #31;"
-	"SUBS R6, R6, #1;"
-	"MOV R8, R8, asr #1;"
-	"BNE .L7;"
+    "ITT ne;"
+    "ADDNE R8, R8, R8, asl #1;"
+    "ADDNE R8, R8, #1;"
+    "ADD R8, R8, R8, lsr #31;"
+    "SUBS R6, R6, #1;"
+    "MOV R8, R8, asr #1;"
+    "BNE .L7;"
 ".L2:MOV R6, R8;"
     "MOV %0, R7;"
-	: "=r" (n)
+    : "=r" (n)
     : "r" (pTailleDonnees)
     :
     );
@@ -92,9 +98,10 @@ Java_com_example_bouhineau_myapplication_MainActivity_stringFromJNICalcVar02(
     return env->NewStringUTF(hello.c_str());
 }
 
+
 extern "C" JNIEXPORT jstring
 JNICALL
-Java_com_example_bouhineau_myapplication_MainActivity_stringFromJNICalcVar(
+Java_edu_bdenis_gperf_MainActivity_stringFromJNICalcVar(
         JNIEnv *env,
         jobject /* this */,
         int pTailleDonnees) {
